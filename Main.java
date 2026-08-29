47d26550408ba1eee1320e9fa6c4b14cc00fa2c5
waleedfiaz663-cpu <waleedfiaz663@gmail.com>
2026-08-29 19:38:09 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 
 public class Main {
+    
     public static void main(String[] args) {
 
        
@@ -15,5 +16,6 @@ public class Main {
         house.addRoom(r3);
 
         house.showRooms();
+        
     }
 }
