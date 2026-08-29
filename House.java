6a522a135c3b9937bd4e1bf0042b2713bb81b24b
waleedfiaz663-cpu <waleedfiaz663@gmail.com>
2026-08-29 19:36:47 +0500

    Update House.java

--- House.java
+++ House.java
@@ -1,29 +1,40 @@
 import java.util.ArrayList;
+
 class House {
+    
     private String name;
     private String address;
     private ArrayList<Room> rooms;
 
     public House(String name, String address) {
+        
         this.name = name;
         this.address = address;
         rooms = new ArrayList<>();
+        
     }
 
     public void addRoom(Room room) {
+        
         rooms.add(room);
+        
     }
 
     public String getAddress() {
+        
         return address;
+        
     }
 
     public void showRooms() {
+        
         System.out.println("\nHouse: " + name);
         System.out.println("Address: " + address);
 
         for (Room room : rooms) {
+            
             room.getInfo();
+            
         }
     }
 }
